458f1d2e7915693b451fe290762ba37d393eef23
KAU <KAU@KAU-PC>
2017-05-29 16:47:19 +0900

    5/29 텀 프로젝트 개선 2차

--- src/Bank.java
+++ src/Bank.java
@@ -2,5 +2,39 @@
 //깃허브와 현재의 프로젝트 연결하기
 //기초적 메인 클래스와 은행 및 가계부 클래스 구현하기
 public class Bank {
-
+	private double interest;
+	private int month;
+	private double money;
+	//각 클래스의 생성자와 그 초기화 함수 만들기 그리고 출력 함수도
+	public Bank(double interest, int month, double money){
+		this.interest=interest;
+		this.month=month;
+		this.money=money;
+	}
+	
+	public void setinterest(double interest){
+		this.interest=interest;
+	}
+	
+	public void setmonth(int month){
+		this.month=month;
+	}
+	
+	public void setmoney(double money){
+		this.money=money;
+	}
+	
+	public void getinterest(){
+		System.out.println("현재의 이자율은 "+interest+"입니다");
+	}
+	
+	public void getmonth(){
+		System.out.println("현재의 경과 기간은 "+month+"입니다");
+	}
+	
+	public void getmoney(){
+		System.out.println("현재의 잔고는 "+money+"입니다");
+	}
+	
+	
 }
--- src/Diary.java
+++ src/Diary.java
@@ -1,4 +1,22 @@
 
 public class Diary {
-
+	private String name;
+	private int price;
+	private int month;
+	
+	//Diary는 생성자 자동초기화 null로
+	
+	public void setname(String name){
+		this.name=name;
+	}
+	
+	public void setprice(int price){
+		this.price=price;
+	}
+	
+	public void setmonth(int month){
+		this.month=month;
+	}
+	
+	//가계부는 출력할 필요가 없다. 저장이 우선이고 월별 품목별 지출금액만 나타내면 충분하다. 
 }
